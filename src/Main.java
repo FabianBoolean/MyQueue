ec1e37b825f77e618bf63bb556258cbec9d3f42c
naibaf911 <fabian.steinhauser1@gmail.com>
2025-03-31 15:21:36 +0200

    Main Klasse mit main Methode zum Testen und für die JAR-Datei wurde hinzugefügt.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Testklasse für die MyLinkedQueue Implementierung.
+ * Führt verschiedene Tests zur Überprüfung der Funktionalität durch.
+ * @author Fabian Steinhauser
+ * @version 31-03-2025
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Erstellen einer neuen Warteschlange
+        MyLinkedQueue<Integer> queue = new MyLinkedQueue<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Test: Elemente anfügen
+        queue.append(10).append(20).append(30);
+        System.out.println("Queue nach Einfügen: " + queue); // [10, 20, 30]
+
+        // Test: Erstes und letztes Element abrufen
+        System.out.println("Erstes Element: " + queue.peek()); // 10
+        System.out.println("Letztes Element: " + queue.peekLast()); // 30
+
+        // Test: Erstes Element entfernen
+        queue.delete();
+        System.out.println("Queue nach Entfernen des ersten Elements: " + queue); // [20, 30]
+
+        // Test: Überprüfen, ob die Queue leer ist
+        System.out.println("Ist die Queue leer? " + queue.isEmpty()); // false
+
+        // Test: Größe der Queue ermitteln
+        System.out.println("Größe der Queue: " + queue.size()); // 2
+
+        // Test: Iteration durch die Queue mit Iterator
+        System.out.println("Durchlaufen der Queue mit Iterator:");
+        for (Integer element : queue) {  // Kein Cast nötig, da MyLinkedQueue Iterable implementiert
+            System.out.println(element);
         }
+
+        // Test: Letztes Element nach einer Löschung abrufen
+        System.out.println("Letztes Element nach Löschen des ersten: " + queue.peekLast()); // 30
     }
 }
